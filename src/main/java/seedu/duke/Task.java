07a9fa7b5d56f0d8a6732da9899502cb3221bcb8
johanesrafael <rafliong95@gmail.com>
2020-09-24 11:59:36 +0800

    Created task class

--- src/main/java/seedu/duke/Task.java
+++ src/main/java/seedu/duke/Task.java
@@ -1,2 +1,24 @@
-package seedu.duke;public class Task {
+package seedu.duke;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "O" : "X"); //return tick or X symbols
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "["+ getStatusIcon() + "]" + getDescription();
+    }
 }
