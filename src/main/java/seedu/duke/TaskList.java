8d6facc7a7e5b24e92457f385f3fe54c4b272f51
johanesrafael <rafliong95@gmail.com>
2020-09-24 12:04:17 +0800

    Created TaskList class

--- src/main/java/seedu/duke/TaskList.java
+++ src/main/java/seedu/duke/TaskList.java
@@ -1,2 +1,30 @@
-package seedu.duke;public class TaskList {
+package seedu.duke;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    final static int MAX_LIST_SIZE = 100;
+    protected static ArrayList<Task> tasks = new ArrayList<>(MAX_LIST_SIZE);
+
+    public static void insertEvent(String userInput) {
+        Event task = Parser.parseEvent(userInput);
+        // assign task into actual task
+        tasks.add(task);
+    }
+
+    public static void insertDeadline(String userInput) {
+        Deadline task = Parser.parseDeadline(userInput);
+        // assign task into actual task
+        tasks.add(task);
+    }
+
+    public static void insertToDo(String userInput) throws ToDoException {
+        ToDo task = Parser.parseToDo(userInput);
+        // assign task into actual task
+        tasks.add(task);
+    }
+
+    static void showNewlyAddedTask() {
+        Ui.getAddedTask();
+    }
 }
