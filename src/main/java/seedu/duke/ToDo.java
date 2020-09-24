9248a1823399318544ff1358cfbbd5ccd04200e2
johanesrafael <rafliong95@gmail.com>
2020-09-24 12:00:08 +0800

    Created ToDo, Event, Deadline classes

--- src/main/java/seedu/duke/ToDo.java
+++ src/main/java/seedu/duke/ToDo.java
@@ -1,2 +1,13 @@
-package seedu.duke;public class ToDo {
+package seedu.duke;
+
+public class ToDo extends Task{
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString() ;
+    }
+
 }
