d25398480d5c7712a15297076f28058d666416a4
johanesrafael <rafliong95@gmail.com>
2020-10-05 18:48:23 +0800

    Implement FocusActionTest

--- src/test/java/command/action/FocusActionTest.java
+++ src/test/java/command/action/FocusActionTest.java
@@ -1,4 +1,26 @@
+package command.action;
+
+import constants.Constants;
+import org.junit.jupiter.api.Test;
+import seedu.duke.Duke;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Focus Action Test
+ */
 class FocusActionTest {
-  
+    private String[] testCommand = {"focus", "focus deadline", "focus todo", "focus event"};
+
+    @Test
+    void act_customFocusInput_focussedlist() {
+        Duke d = new Duke(false, System.out, System.in, Constants.PATH, Constants.FILENAME);
+        assertAll("testCommand", () -> assertTrue(d.testSut(testCommand[0]).contains(Constants.NO_TASK_TYPE)),
+                () -> assertTrue(d.testSut(testCommand[1]).contains("by")),
+                () -> assertTrue(d.testSut(testCommand[2]).contains("T")),
+                () -> assertTrue(d.testSut(testCommand[3]).contains("at"))
+        );
+
+    }
+    
 }
