79fde60d251c0455c7246b5558bd2775585d9144
Li Bozhao <libozhao142857@hotmail.com>
2020-10-07 23:10:32 +0800

    Added TodoActionTest JUnit test

---
+++ src/test/java/command/action/TodoActionTest.java
@@ -0,0 +1,26 @@
+package command.action;
+
+
+import constants.Constants;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import seedu.duke.Duke;
+
+/**
+ * Module Credits Test.
+ */
+public class TodoActionTest {
+
+    private String[] testCommand = {"todo abc", "todo def"};
+
+    @Test
+    public void act_todoCommandsInputs_todoCreated() {
+        Duke d = new Duke(false, System.out, System.in, Constants.PATH, Constants.FILENAME);
+
+        assertAll("TodoActionTest", () -> assertTrue(d.testSut(testCommand[0]).contains("abc")),
+                () -> assertTrue(d.testSut(testCommand[1]).contains("def"))
+        );
+    }
+}
