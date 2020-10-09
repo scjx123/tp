759e4de709ac9e9b40ba8f51947157b75f512830
Li Bozhao <libozhao142857@hotmail.com>
2020-10-09 14:06:50 +0800

    Greatly Simplify

--- src/main/java/command/action/DetailAction.java
+++ src/main/java/command/action/DetailAction.java
@@ -1,17 +1,6 @@
 package command.action;
 
-import command.ParamNode;
-import constants.Constants;
 import data.Item;
-import data.SingleModule;
-import data.Data;
-import data.jobs.Task;
-import exceptions.CommandException;
-import exceptions.InvalidCommandException;
-import exceptions.ModuleNotFoundException;
-
-import java.net.MalformedURLException;
-import java.util.ArrayList;
 
 public class DetailAction extends TakeAction {
 
