6f827a8a063a61b6532df8f46c7550ddcc6bd393
Li Bozhao <libozhao142857@hotmail.com>
2020-10-09 16:14:18 +0800

    Formatting

--- src/main/java/command/action/UnselectAction.java
+++ src/main/java/command/action/UnselectAction.java
@@ -2,7 +2,7 @@ package command.action;
 
 import data.Item;
 
-public class UnselectAction extends SelectAction{
+public class UnselectAction extends SelectAction {
     @Override
     protected void modifyObject(Item item) {
         item.isSelected = false;
--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,7 +1,32 @@
 package constants;
 
 
-import command.action.*;
+import command.action.Action;
+import command.action.AddAction;
+import command.action.ByeAction;
+import command.action.CalculateCapAction;
+import command.action.ClearAction;
+import command.action.DeadlineAction;
+import command.action.DeleteAction;
+import command.action.DetailAction;
+import command.action.DoneAction;
+import command.action.EventAction;
+import command.action.FancyAction;
+import command.action.FindAction;
+import command.action.FocusAction;
+import command.action.HelpAction;
+import command.action.ListAction;
+import command.action.McAction;
+import command.action.NextAction;
+import command.action.PlainAction;
+import command.action.PrevAction;
+import command.action.SelectAction;
+import command.action.TakeAction;
+import command.action.TodoAction;
+import command.action.UndoneAction;
+import command.action.UnknownAction;
+import command.action.UnselectAction;
+import command.action.UntakeAction;
 
 import java.util.Map;
 
