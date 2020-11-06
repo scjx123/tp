effdf205de67ceacde4ff7f8d40a6429bf27c1ad
Li Bozhao <libozhao142857@hotmail.com>
2020-11-07 03:10:29 +0800

    Closes #262 : fix format
    Closes #262

--- src/main/java/exceptions/MissingDescriptionException.java
+++ src/main/java/exceptions/MissingDescriptionException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 import constants.Constants;
 
-public class MissingDescriptionException extends CommandException{
+public class MissingDescriptionException extends CommandException {
     @Override
     public String getMessage() {
         return Constants.NO_DESCRIPTION;
