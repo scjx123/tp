935453397d9df6a90b934a10b94117f6baf0fc53
cococrunch <adinatatan99@gmail.com>
2020-11-08 05:44:36 +0800

    Edit Helptext and Minor test

--- src/test/java/command/action/GoalActionTest.java
+++ src/test/java/command/action/GoalActionTest.java
@@ -1,42 +1,52 @@
-/*
 package command.action;
 
-import data.DataDummy;
-import data.SingleModule;
+import constants.Constants;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import seedu.duke.Domsun;
 
 class GoalActionTest {
-    DataDummy dataTest = new DataDummy();
+
+    private Domsun domsun = new Domsun(false, System.out, System.in, Constants.PATH,
+        Constants.TEST_TASK_FILENAME, Constants.TEST_COURSE_FILENAME);
+
+    private String[] testCommand =
+        {"goal -total 160 4.6 -taken 100 4.5",
+            "goal -total 160 4.9 -taken 100 4.5",
+            "goal -total 220 4.9 -taken 225 4.5",
+            "goal -total 160 4.9 -taken 159 5.0",
+            "goal -total 160 4.8"
+        };
 
     @Test
-    public void act_customUserData_calculatedCap() throws Exception {
-        GoalAction action = new GoalAction();
-        action.modulesWithGrades.put("CS1231", 5.0);
-        action.modulesWithGrades.put("MA1511", 4.5);
-        action.modulesWithGrades.put("CS2040C", 3.0);
-        action.option = "c";
-        action.targetCap = 4.8;
-        action.targetMC = 160;
-        String expectedOutput = "Your required average CAP is: 4.8\r\n" + "Jia you! :D\r\n";
-        assertEquals(expectedOutput, action.act(dataTest), "Goal normal_custom_data fails");
-    }
+    public void act_customModuleInputs_calculatedCapValue() {
+        String[] expectedOutput = {
+            "Your required average CAP is: 4.77\r\n"
+                + "Try \"cap\" to see your current cap!\r\n"
+                + "Jia you! :D\r\n",
 
-    @Test
-    public void act_normalUserData_calculatedCA() throws Exception {
-        int[] indices = {1, 2, 3};
-        String[] grades = {"A", "B", "C"};
-        for (int index : indices) {
-            SingleModule module = (SingleModule) dataTest.mods.get(index);
-            module.isTaken = true;
-            module.grade = grades[index - 1];
-        }
-        GoalAction action = new GoalAction();
-        action.option = "u";
-        action.targetCap = 4.3;
-        action.targetMC = 160;
-        String expectedOutput = "Your required average CAP is: 4.36\r\n" + "Jia you! :D\r\n";
-        assertEquals(expectedOutput, action.act(dataTest), "Goal normal_user_data fails");
+            "Your required average CAP is: 5.57\r\n"
+                + "Try \"cap\" to see your current cap!\r\n"
+                + "Looks like the target is a bit far away QAQ\r\n",
+
+            "You have taken MC sufficient for your graduation! Goal is reached :D\r\n",
+
+            "Your required average CAP is: -11\r\n"
+                + "Try \"cap\" to see your current cap!\r\n"
+                + "This means you need to lower your grade to achieve your goal. Tank the bell curve for us! :D\r\n",
+        };
+
+        String testCustomInputsCommand = "cap CS2113 A CG1112 A-";
+        assertAll("Goal action test",
+            () -> assertEquals(expectedOutput[0], domsun.testSut(testCommand[0], false, true),
+                "Within target"),
+            () -> assertEquals(expectedOutput[1], domsun.testSut(testCommand[1], false, true),
+                "High target"),
+            () -> assertEquals(expectedOutput[2], domsun.testSut(testCommand[2], false, true),
+                "Reached goal"),
+            () -> assertEquals(expectedOutput[3], domsun.testSut(testCommand[3], false, true),
+                "Low target")
+        );
     }
 }
-*/
